19e492f94d812d57227cfe981f662fee1e1209ed
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-03-02 19:22:01 -0500

    Added new screen

---
+++ SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  LoginProject
+//
+//  Created by Kritima Kukreja on 2020-03-02.
+//  Copyright © 2020 Kritima Kukreja. All rights reserved.
+//
+
+import Foundation
